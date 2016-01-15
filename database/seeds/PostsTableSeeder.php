7788fd7450b49f4aaca6d40967566351d4a0e123
hiroki0325 <nexseed.shimada@gmail.com>
2016-01-15 16:28:26 +0800

    テスト＆postシーダー作成

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->truncate();
+
+        DB::table('posts')->insert([
+            [
+                'title'      => '困った',
+                'message'       => 'When there is no desire, all things are at peace.',
+                'code'       => '',
+                'member_id' => '1',
+                'created_at' => '2015-01-31 23:59:59',
+                'updated_at' => '2015-01-31 23:59:59',
+            ],
+            [
+                'title'      => 'えーー',
+                'message'       => 'When there is no desire, all things are at peace.',
+                'code'       => '',
+                'member_id' => '2',
+                'created_at' => '2015-01-31 23:59:59',
+                'updated_at' => '2015-01-31 23:59:59',
+            ],
+            [
+                'title'      => 'むりぽ',
+                'message'       => 'When there is no desire, all things are at peace.',
+                'code'       => '',
+                'member_id' => '1',
+                'created_at' => '2015-01-31 23:59:59',
+                'updated_at' => '2015-01-31 23:59:59',
+            ],
+                ]);
+
+
+    }
+}
---
+++ tests/PostTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostTest extends TestCase
+{
+    // アクセスした際の表示周りのテスト
+    public function testAccess()
+    {
+        $this->visit('/')
+               ->see('会員登録') // app.blade.php(レイアウトファイル)が表示されているか
+               ->see('新着コード') // index.blade.phpが表示されているか
+               ->see('困った') // データベースに登録してあるデータが表示されているか
+               ->click('会員登録') // 会員登録ボタンを押下
+               ->seePageIs('/auth/register'); // 会員登録ボタンを押下した際の遷移先が正しいか
+    }
+
+}
